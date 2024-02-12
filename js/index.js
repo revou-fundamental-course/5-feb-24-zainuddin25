9f928597912aaab061ae5854dac4178695afe01f
Muhammad Zainuddin Basyar <tech_dev2@indicocompany.com>
2024-02-12 20:57:40 +0700

    ADD : Function JS to convert Celcius to Fahrenheit and Fahrenheit to Celcius

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const celciusToFahrenheit = (celcius) => {
+  return (celcius * 9) / 5 + 32;
+};
+
+const fahrenheitToCelcius = (fahrenheit) => {
+  return ((fahrenheit - 32) * 5) / 9;
+  //   return (fahrenheit - 32) * (5 / 9);
+};
