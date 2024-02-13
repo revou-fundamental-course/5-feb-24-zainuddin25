1eb4655f2fd32d642a1ecffb7e9260c9e9beb1ce
Muhammad Zainuddin Basyar <tech_dev2@indicocompany.com>
2024-02-13 21:05:03 +0700

    ADD : handler for submit form

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 const navigationResponsive = document.querySelector("#navigation-responsive");
+const inputNumber = document.querySelector("#input-form");
 
 document
   .querySelector("#hamburger-menu")
   .addEventListener("click", function () {
     navigationResponsive.classList.toggle("active-menu");
+  });
+
+document
+  .querySelector("#form-converter")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+
+    const number = formData.get("number");
+
+    alert(number);
   });
 
 const celciusToFahrenheit = (celcius) => {
