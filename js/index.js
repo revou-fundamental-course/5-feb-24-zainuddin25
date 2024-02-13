9ac582175886478e392244025a5ae78cef9ec7aa
Muhammad Zainuddin Basyar <tech_dev2@indicocompany.com>
2024-02-13 22:02:56 +0700

    ADD : add history in localstorage

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const inputNumber = document.querySelector("#input-form");
 const buttonConvert = document.querySelector("#button-convert");
 const inputResult = document.querySelector("#input-result");
 const converterFormula = document.querySelector("#converter-formula");
+const history = localStorage.getItem("history");
 
 const celciusToFahrenheit = (celcius) => {
   return (celcius * 9) / 5 + 32;
@@ -24,13 +25,23 @@ document
     event.preventDefault();
 
     const formData = new FormData(this);
-
     const number = formData.get("number");
+    const result = celciusToFahrenheit(number);
+
+    const existingData = JSON.parse(localStorage.getItem("history")) || [];
+
+    const newHistory = {
+      from: "celcius",
+      to: "fahrenheit",
+      number: number,
+      result: result,
+    };
 
     if (number != "") {
-      const result = celciusToFahrenheit(number);
       inputResult.value = result;
       converterFormula.value = `(${number}°C * 9 / 5) + 32 = ${result}°F`;
+      existingData.push(newHistory);
+      localStorage.setItem("history", JSON.stringify(existingData));
     } else {
       alert("Please input the number in number section input");
       inputResult.value = "";
