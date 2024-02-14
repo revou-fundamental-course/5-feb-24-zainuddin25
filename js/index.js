83ec958f42d6148256375387f9ab991b3bf4401b
Muhammad Zainuddin Basyar <tech_dev2@indicocompany.com>
2024-02-14 16:10:07 +0700

    UPDATE : and select to change converter type

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 const navigationResponsive = document.querySelector("#navigation-responsive");
-const inputNumber = document.querySelector("#input-form");
-const buttonConvert = document.querySelector("#button-convert");
+const inputNumber = document.querySelector("#input-from");
 const inputResult = document.querySelector("#input-result");
+const buttonConvert = document.querySelector("#button-convert");
 const converterFormula = document.querySelector("#converter-formula");
+const titleFrom = document.querySelector("#title-from");
+const titleResult = document.querySelector("#title-result");
+
 const history = localStorage.getItem("history");
+let typeConvert = "ctf";
 
 const celciusToFahrenheit = (celcius) => {
   return (celcius * 9) / 5 + 32;
@@ -12,6 +16,24 @@ const celciusToFahrenheit = (celcius) => {
 const fahrenheitToCelcius = (fahrenheit) => {
   return ((fahrenheit - 32) * 5) / 9;
 };
+
+document
+  .querySelector("#select-type-convert")
+  .addEventListener("change", function (event) {
+    typeConvert = event.target.value;
+
+    inputNumber.value = "";
+    inputResult.value = "";
+    converterFormula.value = "";
+
+    if (typeConvert == "ctf") {
+      titleFrom.textContent = "Celcius (°C)";
+      titleResult.textContent = "Fahrenheit (°F)";
+    } else {
+      titleFrom.textContent = "Fahrenheit (°F)";
+      titleResult.textContent = "Celcius (°C)";
+    }
+  });
 
 document
   .querySelector("#hamburger-menu")
@@ -26,22 +48,32 @@ document
 
     const formData = new FormData(this);
     const number = formData.get("number");
-    const result = celciusToFahrenheit(number);
+    let result = 0;
+
+    if (typeConvert == "ctf") {
+      result = celciusToFahrenheit(number);
+    } else {
+      result = fahrenheitToCelcius(number);
+    }
 
     const existingData = JSON.parse(localStorage.getItem("history")) || [];
 
     const newHistory = {
-      from: "celcius",
-      to: "fahrenheit",
+      from: typeConvert == "ctf" ? "Celcius" : "Fahrenheit",
+      to: typeConvert == "ctf" ? "Fahrenheit" : "Celcius",
       number: number,
       result: result,
     };
 
     if (number != "") {
       inputResult.value = result;
-      converterFormula.value = `(${number}°C * 9 / 5) + 32 = ${result}°F`;
       existingData.push(newHistory);
       localStorage.setItem("history", JSON.stringify(existingData));
+      if (typeConvert == "ctf") {
+        converterFormula.value = `(${number}°C * 9 / 5) + 32 = ${result}°F`;
+      } else {
+        converterFormula.value = `(${number}°F - 32) * 5 / 9 = ${result}°C`;
+      }
     } else {
       alert("Please input the number in number section input");
       inputResult.value = "";
